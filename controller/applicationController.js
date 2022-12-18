6eebdb0aa63a42f2859672d37c277c4699266aef
Pratik Vansh <105606563+pbvansh@users.noreply.github.com>
2022-12-18 17:32:07 +0000

    fix bugs

--- controller/applicationController.js
+++ controller/applicationController.js
@@ -3,48 +3,58 @@ const asyncHandler = require("express-async-handler")
 const Application = require("../model/applicationModel")
 
 const createApplication = asyncHandler(async (req, res) => {
-    const capp = await Application.create(req.body)
-    res.status(200).json(capp)
+  const capp = await Application.create(req.body)
+  res.status(200).json(capp)
 })
 
 const updateApplication = asyncHandler(async (req, res) => {
-    const uapp = await Application.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    res.status(200).json(uapp)
+  const uapp = await Application.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  res.status(200).json(uapp)
 })
 
+const checkIsAlreadyApplied = asyncHandler(async (req, res) => {
+  const { user_id, Internship_id } = req.body
+  const app = await Application.find({ user_id, Internship_id })
+  if (app.length > 0) {
+    res.status(200).json({ applied: true })
+  } else {
+    res.status(200).json({ applied: false })
+  }
 
+})
 const getApplication = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const uapp = await Application.find({ user_id: id }).populate({
-        path: "Internship_id",
-        populate: {
-            path: "company_id",
-            select: ['Name','email','Contact']
-        }
-        , select: 'Name Internship_Name TotalNoOfApplicants'
-    })
+  const id = req.params.id;
+  const uapp = await Application.find({ user_id: id }).populate({
+    path: "Internship_id",
+    populate: {
+      path: "company_id",
+      select: ['Name', 'email', 'Contact']
+    }
+    , select: 'Name Internship_Name TotalNoOfApplicants'
+  })
 
-    res.status(200).json(uapp)
+  res.status(200).json(uapp)
 })
 
 const getCoverLetter = asyncHandler(async (req, res) => {
-    // console.log(req.params.id);
-    const letter = await Application.findById(req.params.id)
+  // console.log(req.params.id);
+  const letter = await Application.findById(req.params.id)
 
-    res.status(200).json(letter)
+  res.status(200).json(letter)
 })
 
 const getTotalNUmberOfApplicant = asyncHandler(async (req, res) => {
-    const Internship_id = req.params.id;
-    const total = await Application.countDocuments({Internship_id})
-    res.status(200).json(total)
+  const Internship_id = req.params.id;
+  const total = await Application.countDocuments({ Internship_id })
+  res.status(200).json(total)
 })
 
 
 module.exports = {
-    createApplication,
-    updateApplication,
-    getApplication,
-    getCoverLetter,
-    getTotalNUmberOfApplicant
+  createApplication,
+  updateApplication,
+  getApplication,
+  getCoverLetter,
+  getTotalNUmberOfApplicant,
+  checkIsAlreadyApplied
 }
--- controller/companyController.js
+++ controller/companyController.js
@@ -7,9 +7,9 @@ const Internship = require("../model/internshipModel")
 const Application = require('../model/applicationModel')
 
 const signupEmployee = asyncHandler(async (req, res) => {
-    console.log(req.body)
-    const user = await Company.create(req.body)
-    res.status(200).json(user)
+  nsole.log(req.body)
+  const user = await Company.create(req.body)
+  s.status(200).json(user)
 
 })
 
@@ -20,59 +20,64 @@ const signupEmployee = asyncHandler(async (req, res) => {
 // })
 
 const loginEmployee = asyncHandler(async (req, res) => {
-    //    const user = await Company.findOne({Email, Password})  
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.send(400)
-        throw new Error("Please enter the correct email and password")
-    }
-    //l
+  //    const user = await Company.findOne({Email, Password})  
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.send(400)
+    throw new Error("Please enter the correct email and password")
+  }
+  const user = await Company.findOne({ email })
+  if (user && await bcpt.compare(password, user.password)) {
+    res.status(200).json({
+      email: user._doc.email,
+      Fname: user._doc.First_name,
+      token: user.genToken(),
+      isAuth: true,
+      isEmp: true
+    })
+  }
+  else {
+    res.status(200).json({
+      isAuth: false,
+      msg: "invalid details"
+    })
 
-    const user = await Company.findOne({ email })
-    if (user && await bcpt.compare(password, user.password)) {
-        res.status(200).json({
-            email: user._doc.email,
-            Fname: user._doc.First_name,
-            token: user.genToken(),
-            isAuth: true,
-            isEmp: true
-        })
-    }
-
-    else {
-        res.status(200).json({
-            isAuth : false,
-            msg : "invalid details"
-          })
-
-    }
+  }
 
 })
+
 
 const addOtherDetails = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    const company = await Company.findByIdAndUpdate(id, req.body, { new: true });
-    res.status(200).json(company)
+  const id = req.params.id;
+  const company = await Company.findByIdAndUpdate(id, req.body, { new: true });
+  res.status(200).json(company)
 })
+
+const getOtherDetails = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const company = await Company.findById(id).select(['Name', 'About_company', 'URL']);
+  res.status(200).json(company);
+});
 
 const getEmpInternship = asyncHandler(async (req, res) => {
-    const company_id = req.params.id;
-    const internship = await Internship.find({ company_id }).populate('company_id', ["Name", "About_company"])
-    res.status(200).json(internship)
-})
+  const company_id = req.params.id;
+  const internship = await Internship.find({ company_id }).populate('company_id', ["Name", "About_company"])
+  res.status(200).json(internship)
+});
 
 const getApplicantsDetails = asyncHandler(async (req, res) => {
-    const Internship_id = req.params.id;
-    const applicants = await Application.find({ Internship_id }).populate('user_id', ['firstName', 'lastName']);
-    res.status(200).json(applicants);
-})
+  const Internship_id = req.params.id;
+  const applicants = await Application.find({ Internship_id }).populate('user_id', ['firstName', 'lastName']);
+  res.status(200).json(applicants);
+});
 
 module.exports = {
-    signupEmployee,
-    loginEmployee,
-    addOtherDetails,
-    getEmpInternship,
-    getApplicantsDetails
+  signupEmployee,
+  loginEmployee,
+  addOtherDetails,
+  getEmpInternship,
+  getApplicantsDetails,
+  getOtherDetails
 }
 
 
